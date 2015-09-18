8255ab43d6145548ebf8807ac0e60e0f436d76d6
Justin Schaefer <justins@cpanel.net>
2015-09-18 14:16:27 -0500

    Reduced needs of nito.pl to new() and run()

--- nito.pl
+++ nito.pl
@@ -1,20 +1,12 @@
 #!/usr/bin/perl
 # Nito: A Simple Bot Written in Perl
+# This is simply a kickoff script. Calling new will build the object
 # Source: http://oreilly.com/pub/h/1964
 
 use strict;
 use Nito;
-use IO::Socket::INET;
 
-print "Gravelord Nito bot ver. 0.5\n";
+print "Gravelord Nito bot ver. 0.6\n";
 
-my %opts = Nito::load_config();
-
-my $socket = IO::Socket::INET->new(
-	PeerAddr => $opts{server},
-	PeerPort => $opts{port},
-	Proto => 'tcp')
-		or die "Can't connect to $opts{server}:$opts{port}! $!.\n";
-
-my $nito = Nito->new( $socket, $opts{nick}, $opts{user} );
+my $nito = Nito->new();
 $nito->run();
